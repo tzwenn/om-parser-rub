37ff434228f5eee2a6678004095d11e7d3d683e6
Sven Koehler <sven.koehler@hpi.de>
2023-04-23 13:17:18 +0200

    Hacky notes-translation

--- rub/parser.py
+++ rub/parser.py
@@ -14,6 +14,27 @@ PRICE_ROLES = ('student', 'other')
 
 LOG = logging.getLogger(__name__)
 
+
+def parse_notes_block(notes_block_div):
+	if not notes_block_div:
+		return {}
+	notes_list = [
+		e.strip() for e in 
+		notes_block_div.string.partition(': ')[2].split(',')
+	]
+	return {k.lstrip('('): v.strip() for k,v in (e.split(')', 2) for e in notes_list)}
+
+
+def parse_notes(notes_div):
+	res = {}
+	if notes_div:
+		for block in notes_div.find_all('div', 'kennzeichen'):
+			b = parse_notes_block(block)
+			res.update(b)
+	
+	return res
+
+
 def parse_meal(meal_div):
 	title_span = meal_div.find('span', 'live_speiseplan_item_title')
 	# All `live_speiseplan_item_title` entries that have a sub (thus no valid string), 
@@ -43,8 +64,13 @@ def parse_day(day_div):
 			yield date, MEAL_NAME_TEMPLATE % meal_id, *m
 
 
+def translate_notes(_date, _meal, _title, notes, _prices, notes_dict):
+	return _date, _meal, _title, [notes_dict.get(n, n) for n in notes], _prices
+
+
 def download_menu():
 	request = requests.get(URL, timeout=30)
 	soup = BeautifulSoup(request.text, 'html.parser')
+	notes_dict = parse_notes(soup.find('div', 'live_speiseplan_kennzeichnung_content'))
 	for day_div in soup.find_all('div', 'live_speiseplan_single_day'):
-		yield from parse_day(day_div)
+		yield from map(lambda t: translate_notes(*t, notes_dict), parse_day(day_div))
